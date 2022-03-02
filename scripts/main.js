76f6edede62e0155916b77d458bc1c2ab7ccdfea
João Vitor Nunes do Amaral <72924450+nunesAmaral@users.noreply.github.com>
2022-03-01 22:41:52 -0300

    adicionado script 'menu-mobile'

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import MenuMobile from "./menu-mobile.js";
+const menuMobile = new MenuMobile('#btn-menu', '#hero nav ul', 'assets/activeBtn.svg');
+menuMobile.init();
--- scripts/menu-mobile.js
+++ scripts/menu-mobile.js
@@ -1,3 +1,21 @@
-//ao clique no btn-menu, adicionar classe ativo à nav.
-
-const menuList = document.querySelector('')
+export default class MenuMobile {
+  constructor(btnMenu, menuList, srcActiveBtn){
+    this.btnMenu = document.querySelector(btnMenu);
+    this.menuList = document.querySelector(menuList);
+    this.srcActiveBtn = srcActiveBtn;
+  }
+  toggleMenu() {
+    this.btnMenu.addEventListener('click', () => {
+      this.menuList.classList.toggle('active');
+      this.toggleIcon()
+    });
+  }
+  toggleIcon() {
+    if(this.menuList.contains('active')){
+      this.btnMenu.setAtribute('src', this.srcActiveBtn);
+    } 
+  }
+  init(){
+    this.toggleMenu();
+  }
+}
